703e5a2f015215d5409386480a8f3ff3f32245af
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-26 13:32:52 +0100

    26022024 v.2

---
+++ 10. - 26022024/smajlik.c
@@ -0,0 +1,36 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 29.01.2024 14:19:41
+ * Author : stepan.krousky
+ */ 
+
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRA=0b11111111;
+	DDRC=0b11111111;
+	PORTD=0b10000000;
+	
+	while (1)
+	{
+		PORTA=0b00000010;
+		PORTC=0b11011011;
+		
+		PORTA=0b00000100;
+		PORTC=0b11011011;
+		
+		PORTA=0b00001000;
+		PORTC=0b11011011;
+		
+		PORTA=0b00100000;
+		PORTC=0b10111101;
+		
+		PORTA=0b01000000;
+		PORTC=0b11000011;
+	}
+}
